1e2f361e38d2a2498752fd4f17baead32a730143
Wataru Mizuno <watarumizuno1128@gmail.com>
2024-04-17 13:38:16 +0900

    practice: day3: use black screen output

--- practice/day3/haribote.asm
+++ practice/day3/haribote.asm
@@ -1,26 +1,8 @@
 ;  haribote-os
-	ORG	0xc200
-	MOV	SI, msg
-putloop:
-	MOV	AL,[SI]
-	ADD	SI,1		; Increment
-	CMP	AL,0
-	JE	fin
-	MOV	AH, 0x0e	; Function to show 1 character
-	MOV	BX,15		; Color code
-	INT	0x10		; Call video BIOS
-	JMP	putloop
-
+	ORG	0xc200		; Program load location
+	MOV	AL, 0x13	; VGA graphics 320x200x8bit color setting for BIOS
+	MOV	AH, 0x00
+	INT	0x10		; Call BIOS
 fin:
-	HLT			; Stop CPU till interrupt
-	JMP	fin		; Infinite loop
-
-msg:
-	DB	0x0a, 0x0a	; 2 new lines
-	DB	"load haribote OS"
-	DB	0x0a		; 1 new line
-	DB	0
-
-	RESB	0x1fe-($-$$)
-
-	DB	0x55, 0xaa
+	HLT
+	JMP	fin
