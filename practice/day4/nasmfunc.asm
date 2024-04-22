d13832d2625f02e941af94b89ab686c06340a8e7
Wataru Mizuno <watarumizuno1128@gmail.com>
2024-04-22 23:34:37 +0900

    practice: day4: change display color white
    Fill 15 to VRAM to change display output.

--- practice/day4/nasmfunc.asm
+++ practice/day4/nasmfunc.asm
@@ -1,6 +1,12 @@
 ;;; naskfunc
 section .text
 	GLOBAL io_hlt
+	GLOBAL write_mem8
 io_hlt:
 	HLT
+	RET
+write_mem8:	; void write_mem8(int addr, int data);
+	MOV	ECX, [ESP+4]	; Mov 1st arg to ECX
+	MOV	AL, [ESP+8]	; Mov second arg to AL
+	MOV	[ECX], AL
 	RET
