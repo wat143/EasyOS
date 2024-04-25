db3427abd263ee912d3f097f98269f8f77ce9ae4
Wataru Mizuno <watarumizuno1128@gmail.com>
2024-04-25 15:12:05 +0900

    practice: day5: implement sprintf
    Since i686 compiler failed to link sprintf, implemented as util.
    This sprintf only supports %d as of now.
    
    Note: 2nd args shall be ended by '\n' so that function ends while loop correctly.

--- practice/day5/bootpack.c
+++ practice/day5/bootpack.c
@@ -1,4 +1,5 @@
 #include "hankaku.h"
+#include "utils.h"
 
 void io_hlt(void);
 void io_cli(void);
@@ -41,12 +42,15 @@ void HariMain(void)
   char *vram;
   int xsize, ysize;
   struct BOOTINFO *binfo = (struct BOOTINFO*)0x0ff0;
+  char s[40] = {'\0'};
 
   init_palette();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
   putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
   putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
   putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
+  sprintf(s, "scrnx = %d\n", binfo->scrnx);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
   for (;;) {
     io_hlt(); // Call io_hlt defined in naskfunc.asm
